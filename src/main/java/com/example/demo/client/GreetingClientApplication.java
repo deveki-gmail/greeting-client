2d3f6ebfe1b300151e2e5b5f992f9fbaad4e51be
deveki-gmail <devekinandan@gmail.com>
2022-02-28 18:16:37 +0530

    code changed

--- src/main/java/com/example/demo/client/GreetingClientApplication.java
+++ src/main/java/com/example/demo/client/GreetingClientApplication.java
@@ -3,24 +3,35 @@ package com.example.demo.client;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @SpringBootApplication
 public class GreetingClientApplication {
-
+	
+	@Value("${greeting.server.url}")
+	String url;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingClientApplication.class, args);
+		try {
+			System.out.println("Greeting client stared : "+InetAddress.getLocalHost());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	@GetMapping("/hello")
+	@GetMapping("/greet")
 	public String sayHello() throws UnknownHostException, InterruptedException{
-		Thread.sleep(5000l);
-		System.out.println("Request processed successfully at server "+InetAddress.getLocalHost());
-		return "Hello @ "+InetAddress.getLocalHost();
+		System.out.println("Request received at client "+InetAddress.getLocalHost());
+		RestTemplate rt = new RestTemplate();
+		String g = rt.getForObject(url, String.class);
+		return g;
 	}
 
 }
