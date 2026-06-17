61ef5945343f719bec815a009e6219158658e573
deveki nandan mishra <devekinandanmishra@devekis-MacBook-Pro.local>
2026-06-17 13:24:59 +0530

    hero name added in response.

--- src/main/java/com/example/demo/client/GreetingClientApplication.java
+++ src/main/java/com/example/demo/client/GreetingClientApplication.java
@@ -2,6 +2,9 @@ package com.example.demo.client;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -17,21 +20,46 @@ public class GreetingClientApplication {
 	@Value("${greeting.server.url}")
 	String url;
 	
+	String randomName;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingClientApplication.class, args);
 		try {
 			System.out.println("Greeting client stared : "+InetAddress.getLocalHost());
+			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	@GetMapping("/greet")
-	public String sayHello() throws UnknownHostException, InterruptedException{
+	public Map<String, String> sayHello() throws UnknownHostException, InterruptedException{
 		System.out.println("Request received at client "+InetAddress.getLocalHost());
 		RestTemplate rt = new RestTemplate();
+		Map<String, String> map = new HashMap<>();
 		String g = rt.getForObject(url, String.class);
-		return g;
+		map.put("greetingMessage", g);
+		if(randomName == null) 
+		{
+			randomName = names[new Random().nextInt(names.length - 1)];
+		}
+		map.put("toDayHero", randomName);
+		return map;
 	}
-
+	
+	String[] names = {"Subhas Chandra Bose", 
+						"Bhagat Singh",
+						"Sardar Vallabhbhai Patel",
+						"Rani Lakshmibai",
+						"Bal Gangadhar Tilak",
+						"Lala Lajpat Rai",
+						"Chandrashekhar Azad",
+						"Mangal Pandey",
+						"Madam Bhikaji Cama",
+						"Kittur Chennamma",
+						"Kamaladevi Chattopadhyay",
+						"Shri Ram",
+						"Shri Krishna",
+						"Deveki Nandan"
+					 };
 }
